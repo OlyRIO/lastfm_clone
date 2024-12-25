71f3bcd8bde582ef71d0861849aec2e755fdbc79
Mihailo Benington <mihailo.benington@gmail.com>
2024-12-25 19:33:44 +0100

    Added a method for generating data with faker and a config file to separate database login info. Updated .gitignore NOT TO upload the database login info.

---
+++ data_generator.py
@@ -0,0 +1,30 @@
+from faker import Faker
+import random
+
+def generate_spotify_data(num_artists=5, albums_per_artist=3, songs_per_album=10):
+    faker = Faker()
+    
+    data = []
+    for _ in range(num_artists):
+        artist = {
+            "name": faker.name(),
+            "genre": random.choice(["Pop", "Rock", "Hip-Hop", "Jazz", "Classical", "Electronic", "R&B", "Reggae"]),
+            "followers": faker.random_int(min=1000, max=1000000),
+            "albums": []
+        }
+        for _ in range(albums_per_artist):
+            album = {
+                "title": faker.catch_phrase(),
+                "release_date": faker.date_time_this_year(), # faker.date_between(start_date='-10y', end_date='today'),
+                "songs": []
+            }
+            for _ in range(songs_per_album):
+                song = {
+                    "title": faker.sentence(nb_words=3).strip("."),
+                    "duration": random.randint(120, 300),  # Duration in seconds
+                    "popularity": random.randint(1, 100)
+                }
+                album["songs"].append(song)
+            artist["albums"].append(album)
+        data.append(artist)
+    return data

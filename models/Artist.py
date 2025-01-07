5123113594fdfb3f6840d5487c2b2adecd8c9aff
Mihailo Benington <mihailo.benington@gmail.com>
2025-01-07 22:43:58 +0100

    Added an artist model.

---
+++ models/Artist.py
@@ -0,0 +1,26 @@
+from db_helper import *
+
+# User class for Flask-Login
+class Artist():
+    def __init__(self, id, name, genre, followers, albums):
+        self.id = id
+        self.name = name
+        self.genre = genre
+        self.followers = followers
+        self.albums = albums
+
+    @staticmethod
+    def get(user_id):
+        artist_data = artists_collection.find_one({"_id": user_id})
+        
+        if artist_data:
+            return Artist(
+                str(artist_data["_id"]),
+                artist_data["name"],
+                artist_data["genre"],
+                artist_data["followers"],
+                artist_data["albums"])
+        return None
+    
+    def get_id(self):
+        return self.id
